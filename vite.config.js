98ec9e2ee513f3eeeab5de664584fb5486f607a4
ramco <ramcomp3099@gmail.com>
2026-09-09 09:23:55 +0530

    Rename Pages path to Date_With_Me

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Phone-Link/',
+  base: '/Date_With_Me/',
   plugins: [react()]
 });
